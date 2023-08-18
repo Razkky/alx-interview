c148df5058d232eba3da1ffe4a5263e8d8553c66
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-18 10:38:33 +0100

    complete the interview task of 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A 2D matrix, rotate it 90 degrees clockwise."""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List) -> None:
+    """Rotate a 2D matrix 90 degrees"""
+    length = len(matrix)
+
+    for i in range(length):
+        for j in range(i, length):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(length):
+        matrix[i] = matrix[i][::-1]
